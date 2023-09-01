c692c92c467a787953d4b73b572308899be90dbd
Ivan Shalay <ivanshalay@MacBook-Pro-Ivan.local>
2023-09-01 20:19:03 +0300

    Reworked

--- src/main/java/Lesson_3.java
+++ src/main/java/Lesson_3.java
@@ -2,10 +2,20 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Lesson_3 {
     public static void main(String[] args) {
+        isInSumRange(10, 5);
+        positiveNegative(0);
+        isNegative(1);
+        repeat("Hello", 5);
+        isLeapYear(2004);
+        reverse();
+        oneToHundred();
+        lessThanSix();
+        diagonal();
+        createArray(5, 10);
     }
 
 // Task 1
-    public boolean isInSumRange (int a, int b) {
+    public static boolean isInSumRange (int a, int b) {
         for (int i = 10; i <= 20; i++) {
             if ((a + b) == i) {
                 return true;
@@ -15,7 +25,7 @@ public class Lesson_3 {
     }
 
 // Task 2
-    public void positiveNegative (int a) {
+    public static void positiveNegative (int a) {
         if (a >=0) {
             System.out.println("Положительное");
         } else {
@@ -24,19 +34,19 @@ public class Lesson_3 {
     }
 
 // Task 3
-    public boolean isNegative (int a) {
+    public static boolean isNegative (int a) {
         return a < 0;
     }
 
 // Task 4
-    public void repeat (String line, int count) {
+    public static void repeat (String line, int count) {
         for (int i = 0; i < count; i++) {
             System.out.println(line);
         }
     }
 
 // Task 5
-    public boolean isLeapYear (int year) {
+    public static boolean isLeapYear (int year) {
         if (year < 0) return false;
 
         if ((year % 400) == 0) {
@@ -49,38 +59,46 @@ public class Lesson_3 {
     }
 
 // Task 6
-    int[] array = {1, 1, 0, 1, 0, 0, 1, 0, 0, 1};
-    for (int i = 0; i < array.length; i++) {
-        if (array[i] == 0) {
-            array[i] = 1;
-        } else {
-            array[i] = 0;
+    public static void reverse () {
+        int[] array = {1, 1, 0, 1, 0, 0, 1, 0, 0, 1};
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                array[i] = 1;
+            } else {
+                array[i] = 0;
+            }
         }
     }
 
 // Task 7
-    int[] array2 = new int[100];
-    for (int i = 0; i < array2.length; i++) {
-        array2[i] = i + 1;
+    public static void oneToHundred () {
+        int[] array = new int[100];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
     }
 
 // Task 8
-    int[] array3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-    for (int i = 0 ; i < array3.length; i++) {
-        if (array3[i] < 6) {
-            array3[i] *= 2;
+    public static void lessThanSix () {
+        int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 6) {
+                array[i] *= 2;
+            }
         }
     }
 
 // Task 9
-    int[][] array4 = new int[11][11];
-    for (int i = 0; i < array4.length; i++) {
-        array4[i][i] = 1;
-        array4[i][array4.length - i - 1] = 1;
+    public static void diagonal () {
+        int[][] array = new int[11][11];
+        for (int i = 0; i < array.length; i++) {
+            array[i][i] = 1;
+            array[i][array.length - i - 1] = 1;
+        }
     }
 
 // Task 10
-    public int[] createArray (int len, int initialValue) {
+    public static int[] createArray (int len, int initialValue) {
         int[] array = new int[len];
         for (int i = 0; i < array.length; i++) {
             array[i] = initialValue;
