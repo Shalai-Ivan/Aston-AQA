a3ca483193c43cd827d1f70e86e74a46eb447054
Ivan Shalay <ivanshalay@MacBook-Pro-Ivan.local>
2023-09-01 19:32:48 +0300

    Class renamed

--- src/main/java/Lesson_2.java
+++ src/main/java/Lesson_2.java
@@ -1,8 +1,6 @@
-package org.shalai.ivan;
-
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class Lesson_2 {
     public static void main(String[] args) {
         checkSumSign();
         printColor();
