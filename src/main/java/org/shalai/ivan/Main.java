5c4d56cdbc241101373e8e103094b64249c03340
Ivan Shalay <ivanshalay@MacBook-Pro-Ivan.local>
2023-10-11 17:22:28 +0300

    Finalized

--- src/main/java/org/shalai/ivan/Main.java
+++ src/main/java/org/shalai/ivan/Main.java
@@ -1,10 +1,6 @@
 package org.shalai.ivan;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 public class Main {
     public static void main(String[] args) {
-
     }
 }
